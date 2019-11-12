f331b9548963b3267162a832743737449fb2f867
Karishmakarkii <51119117+Karishmakarkii@users.noreply.github.com>
2019-11-12 21:47:23 +0545

    Update index.js

--- index.js
+++ index.js
@@ -1,58 +1,76 @@
-window.onload=function(){
-	getQuizQuestion();
+window.onload=function () {
+    getQuizQuestion();
+};
+var user_marks=0;
+function getQuizQuestion() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState === 4 && this.status === 200) {
+            var response=JSON.parse(this.responseText).results;
+            onRenderQuiz(response);
+        }else if (this.readyState === 4) {
+            //error
+        }
+    };
+    xhttp.open('get',
+    'https://opentdb.com/api.php?amount=10&category=21&difficulty=easy&type=multiple',true);
+    xhttp.send();
+
 }
-function getQuizQuestion(){
-var xhttp=new XMLHttpRequest();
-		xhttp.onreadystatechange = function(){
-			if(this.readyState===4 && this.status===200){
-				var response=JSON.parse(this.responseText).results;
-				onRenderquiz(response);
-			}else if(this.readyState===4){
-				//error
-			}
-		};
-	xhttp.open('get','https://opentdb.com/api.php?amount=10&category=21&difficulty=easy&type=multiple',true);
-	xhttp.send();
+function onRenderQuiz(data) {
+    var quiz='';
+    data.forEach(function (value,key) {
+        quiz=quiz+'<div><div id="question">' +
+            value.question+
+            '    </div>\n' +
+            '    <div id="answer">' +
+            getAnswerList(value,key)+
+            '    </div></div>'
+    }) ;
+    document.getElementById('quizList').innerHTML=quiz;
+
+}
+function checkCorrectAnswer(answer,correct_answer,aId,qId) {
+    var selectedAnswer=document.getElementById(aId+'-answer-'+qId);
+    if(answer===correct_answer){
+        selectedAnswer.className='correct';
+        user_marks=user_marks+1;
+    }else {
+
+        selectedAnswer.className='incorrect';
+    }
 }
 
-function onRenderquiz(data){
-	var quiz='';
-	data.forEach(function(value){
-		quiz=quiz +'<div><div id="question">'+
-		value.question +
-'</div>'+
-'<div id="answer">'+
-	getAnswerList(value)+
-'</div></div>'
-	});
-	document.getElementById('quizList').innerHTML=quiz;
+function getAnswerList(qList,index) {
+    var answer=qList.incorrect_answers;
+    answer.push(qList.correct_answer);
+    var answers='';
+    shuffle(answer).forEach(function (value,key) {
+         answers= answers+
+    ' <div id="'+key+'-answer-'+index+'" onclick="checkCorrectAnswer(\''+value+'\',\''+qList.correct_answer+'\',\''+key+'\',\''+index+'\')">'+value+'</div>';
+    });
+    return answers;
 }
-function checkCorrectAnswer(answer,correct_answer){
-if(answer==correct_answer){
-	alert('ok')
-}else{
-	alert('not ok')
+function shuffle(arra1) {
+    var ctr = arra1.length, temp, index;
+    while (ctr > 0) {
+        index = Math.floor(Math.random() * ctr);
+        ctr--;
+        temp = arra1[ctr];
+        arra1[ctr] = arra1[index];
+        arra1[index] = temp;
+    }
+    return arra1;
 }
-}
-function getAnswerList(qList){
-	var answer=qList.incorrect_answers;
-	answer.push(qList.correct_answer);
-	var answers=''
-shuffle(answer).forEach(function(answer,key){
-answers=answers+
-'<div onclick="checkCorrectAnswer(\''+answer+'\',\''+qList.correct_answer+'\')">'+answer+'</div>';
-});
-return answers;
-}
-function shuffle(arra1){
-	var ctr=arra1,length,temp,index;
+function showMArks() {
+    document.getElementById('finalMarks').innerHTML=
+        '<div>\n' +
+        '            <span>'+parseInt(user_marks)*10+'</span><br/>\n' +
+        '            <span>-----</span><br/>\n' +
+        '            <span>100</span><br/>\n' +
+        '        </div>';
+    document.getElementById('marks').style.display='block';
+    document.getElementById('quiz').style.display='none';
 
-	while(ctr>0){
-		index=Math.floor(Math.random()*ctr);
-		ctr--;
-		temp=arra1[ctr];
-		arra1[ctr]=arra1[index];
-		arra1[index]=temp;
-	}
-	return arra1;
-}                                                                                                                         
+}
+                                                                                                                    
